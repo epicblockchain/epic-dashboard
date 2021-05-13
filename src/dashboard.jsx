5567467a44132486bb8e1316b382cc723f9ac875
Dan Nicolau <dan.nicolau@protonmail.com>
2021-05-13 15:49:46 -0400

    removed breaking code
    Former-commit-id: a8c778d09a01b80b27513b812b6dcc9e53fa50ad

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -11,46 +11,46 @@ export class Dashboard extends React.Component {
     }
 
     componentDidMount() {
-        let chart = am4core.create("chartdiv", am4charts.XYChart);
-        
-        let hashrateData = {};
-        this.props.data.forEach(miner => {
-            miner.hist.forEach(sample => {
-                if (!(sample.Timestamp in hashrateData)) {
-                    hashrateData[sample.Timestamp] = sample.Hashrate;
-                } else {
-                    hashrateData[sample.Timestamp] += sample.Hashrate;
-                }
-            });
-        });
-        let chartHashrateData = [];
-        for (const seconds in hashrateData) {
-            chartHashrateData.push({
-                time: new Date(seconds * 1000),
-                hashrate: hashrateData[seconds]
-            });
-        }
+        // let chart = am4core.create("chartdiv", am4charts.XYChart);
+        // 
+        // let hashrateData = {};
+        // this.props.data.forEach(miner => {
+        //     miner.hist.forEach(sample => {
+        //         if (!(sample.Timestamp in hashrateData)) {
+        //             hashrateData[sample.Timestamp] = sample.Hashrate;
+        //         } else {
+        //             hashrateData[sample.Timestamp] += sample.Hashrate;
+        //         }
+        //     });
+        // });
+        // let chartHashrateData = [];
+        // for (const seconds in hashrateData) {
+        //     chartHashrateData.push({
+        //         time: new Date(seconds * 1000),
+        //         hashrate: hashrateData[seconds]
+        //     });
+        // }
 
-        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-        let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        let series = chart.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = "hashrate";
-        series.dataFields.dateX = "time";
-        series.strokeWidth = 2;
-        series.smoothing = "monotoneX";
-        series.tooltipText = "Hashrate (TH/s): [bold]{valueY}[/]";
+        // let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        // let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        // let series = chart.series.push(new am4charts.LineSeries());
+        // series.dataFields.valueY = "hashrate";
+        // series.dataFields.dateX = "time";
+        // series.strokeWidth = 2;
+        // series.smoothing = "monotoneX";
+        // series.tooltipText = "Hashrate (TH/s): [bold]{valueY}[/]";
 
-        let bullet = series.bullets.push(new am4charts.CircleBullet());
-        bullet.scale = 0.6;
-        bullet.fill = am4core.color("1b1d4d");
+        // let bullet = series.bullets.push(new am4charts.CircleBullet());
+        // bullet.scale = 0.6;
+        // bullet.fill = am4core.color("1b1d4d");
 
-        this.chart = chart;
+        // this.chart = chart;
     }
 
     componentWillUnmount(){
-        if (this.chart) {
-            this.chart.dispose();
-        }
+        // if (this.chart) {
+        //     this.chart.dispose();
+        // }
     }
 
     render() {
