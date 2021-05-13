62b44f250f24fd22f6ad1b8e28c8947741626c67
Shichao Jiang <sjiang@epicblockchain.io>
2021-05-12 20:34:20 -0400

    Data retrieval pushes instead of replace on update Added more columns to table Added toolbar to table
    Former-commit-id: ab6041d0ddfa18c24437134b206894777fa91510

--- src/app.jsx
+++ src/app.jsx
@@ -3,42 +3,25 @@ const got = require('got');
 const mdns = require('node-dns-sd');
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Sidebar from 'react-sidebar';
 import { Dashboard } from './dashboard.jsx';
-import { DataTable, TestTable } from './table.jsx'
+import { DataTable } from './table.jsx';
 
-import { Menu, MenuItem, MenuDivider } from '@blueprintjs/core';
-import "@blueprintjs/core/lib/css/blueprint.css";
+import { Drawer, ListItem, ListItemIcon, ListItemText, Button, List, Divider } from '@material-ui/core';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import ListAltIcon from '@material-ui/icons/ListAlt';
 
 var miners = [];
-var miner_hist = [];
-
-/*async function test() {
-    var miner_data = [];
-    for (let miner of miners) {
-        try {
-            const data = await got(`http://${miner.address}:${miner.service.port}/summary`);
-            miner_data.push(data.body);
-        } catch(err) {
-            miner_data.push(err);
-        }
-    }
-    sum = miner_data;
-    i++;
-}*/
-
-//browser();
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            sidebarOpen: true,
+            drawerOpen: true,
             page: 'main',
             miner_data: [],
         };
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+        //this.toggleDrawer = this.toggleDrawer.bind(this);
     }
 
     async summary(init) {
@@ -52,17 +35,15 @@ class App extends React.Component {
                     miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: JSON.parse(history.body).History});
                 } else {
                     const lastMHs = JSON.parse(summary.body).Session.LastAverageMHs;
-                    if (lastMHs != null) {
-                        let match = this.state.miner_data.find(a => a.ip == miner.address);
-                        
-                        if (match.hist.length == 0) {
-                            miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: [lastMHs]});
-                        } else if (!match.hist.map(a => a.Timestamp).includes(lastMHs.Timestamp)) {
-                            match.hist.push(lastMHs);
-                            miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: match.hist});
-                        } else {
-                            miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: match.hist});
-                        }
+                    let match = this.state.miner_data.find(a => a.ip == miner.address);
+                    
+                    if (match.hist.length == 0 || lastMHs == null) {
+                        miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: [lastMHs]});
+                    } else if (!match.hist.map(a => a.Timestamp).includes(lastMHs.Timestamp)) {
+                        match.hist.push(lastMHs);
+                        miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: match.hist});
+                    } else {
+                        miner_data.push({ip: miner.address, sum: JSON.parse(summary.body), hist: match.hist});
                     }
                 }
             } catch(err) {
@@ -72,12 +53,22 @@ class App extends React.Component {
         this.setState({miner_data: miner_data});
     }
 
+    compare(a, b) {
+        if (a.address > b.address) return 1;
+        else if (a.address < b.address) return -1;
+        else return 0;
+    }
+
     componentDidUpdate(prevProps, prevState) {
         if (prevState.miner_data != this.state.miner_data) {
             mdns.discover({
                 name: '_epicminer._tcp.local'
             }).then((list) => {
-                miners = list;
+                let prev = miners.map(a => a.address);
+                for (let miner of list) {
+                    if (!prev.includes(miner.address)) miners.push(miner);
+                }
+                
                 setTimeout(() => {
                     this.summary(false);
                     console.log('update');
@@ -87,18 +78,34 @@ class App extends React.Component {
     }
 
     componentDidMount() {
+        /*mdns.startMonitoring().then(() => {
+            console.log('Started');
+        }).catch((err) => {
+            console.error(err);
+        });
+        console.log('mounted');
+        mdns.ondata = (packet) => {
+            for (let ans of packet.answers) {
+                if (ans.name == '_epicminer._tcp.local') {
+                    console.log(packet.address);
+                }
+            }
+        };*/
         mdns.discover({
             name: '_epicminer._tcp.local'
         }).then((list) => {
-            miners = list;
+            miners = list.sort(this.compare);
             this.summary(true);
             console.log('mounted');
         });
     }
 
-    onSetSidebarOpen(open) {
-        this.setState({sidebarOpen: open});
-    }
+    toggleDrawer(open) {
+        /*if (event.type == 'keydown' && (event.key == 'Tab' || event.key == 'Shift')) {
+            return;
+        }*/
+        this.setState({drawerOpen: open});
+    };
 
     setPage(page) {
         this.setState({page: page});
@@ -106,23 +113,26 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Sidebar
-                    sidebar={
-                        <Menu>
-                            <MenuItem text="Main" onClick={() => this.setPage('main')}/>
-                            <MenuItem text="Table" onClick={() => this.setPage('table')}/>
-                        </Menu>
-                    }
-                    open={this.state.sidebarOpen}
-                    onSetOpen={this.onSetSidebarOpen}
-                    styles={{sidebar: {background: 'white'}}}
-                >
-                    <button onClick={() => this.onSetSidebarOpen(true)}>Open Sidebar</button>
-                    { this.state.page == 'main' && <Dashboard data={this.state.miner_data}/> }
-                    { this.state.page == 'table' && <DataTable data={this.state.miner_data}/> }
-                </Sidebar>
-            </div>
+            <React.Fragment>
+                <Button onClick={() => this.toggleDrawer(true)}>Open</Button>
+                <Drawer open={this.state.drawerOpen} onClose={() => this.toggleDrawer(false)}>
+                    <div onClick={() => this.toggleDrawer(false)}>
+                        <List>
+                            <ListItem button key="Dashboard" onClick={() => this.setPage('main')}>
+                                <AssessmentIcon/>
+                                <ListItemText primary="Dashboard"/>
+                            </ListItem>
+                            <ListItem button key="Table" onClick={() => this.setPage('table')}>
+                                <ListAltIcon/>
+                                <ListItemText primary="Table"/>
+                            </ListItem>
+                        </List>
+                    </div>
+                </Drawer>
+
+                { this.state.page == 'main' && <Dashboard data={this.state.miner_data}/> }
+                { this.state.page == 'table' && <DataTable data={this.state.miner_data}/> }
+            </React.Fragment>
         );
     }
 }
--- src/table.jsx
+++ src/table.jsx
@@ -1,72 +1,81 @@
 import * as React from 'react';
-import { Cell, Column, Table } from '@blueprintjs/table';
-import { Tabs, Tab } from '@blueprintjs/core';
-
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/table/lib/css/table.css";
-
-import { DataGrid } from '@material-ui/data-grid';
+import {
+    DataGrid,
+    GridToolbarContainer,
+    GridFilterToolbarButton,
+    GridColumnsToolbarButton,
+    GridDensitySelector } from '@material-ui/data-grid';
+import { Tabs, Tab } from '@material-ui/core';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'ip', headerName: 'IP', width: 130 },
-    { field: 'hashrate15min', headerName: 'Hashrate (15min)', width: 150 }
+    { field: 'ip', headerName: 'IP', width: 120 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'hashrate15min', headerName: 'Hashrate (15min)', width: 180 },
+    { field: 'hashrate6hr', headerName: 'Hashrate (6h)', width: 180 }
 ];
 
-export class DataTable extends React.Component {
-    render() {
-        const rows = this.props.data.map(
-            (a, i) => ({id: i, ip: a.ip, hashrate15min: a.sum.Session['Average MHs']})
-        );
-
-        return (
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-            </div>
-        );
-    }
+function Toolbar() {
+    return(
+        <GridToolbarContainer>
+            <GridColumnsToolbarButton/>
+            <GridFilterToolbarButton/>
+            <GridDensitySelector/>
+        </GridToolbarContainer>
+    );
 }
 
-export class TestTable extends React.Component {
+export class DataTable extends React.Component {
     constructor(props) {
-        super(props);
-        this.renderCell = this.renderCell.bind(this);
-        this.renderCell2 = this.renderCell2.bind(this);
+        super(props)
+        this.state = {selected: []};
     }
 
-    renderCell(row) {
-        return (
-            <Cell>{this.props.data[row].ip}</Cell>
-        );
-    };
-
-    renderCell2(row) {
+    hashrate6hr(row) {
         var sum = 0;
         try {
             for (let obj of this.props.data[row].hist.slice(-6)) {
                 sum += obj.Hashrate;
             }
             sum /= 6;
-        } catch(err) {
-            console.log(err);
+        } catch {
             sum = 'N/A'
         }
-        
-        return (
-            <Cell>{sum}</Cell>
-        );
-    };
+        return sum;
+    }
+
+    select(model) {
+        console.log('before set state', model);
+        var temp = model;
+        this.setState({selected: temp});
+    }
 
     render() {
+        const rows = this.props.data.map(
+            (a, i) => ({
+                id: i,
+                ip: a.ip,
+                name: a.sum.Hostname,
+                hashrate15min: a.sum.Session['Average MHs'],
+                hashrate6hr: this.hashrate6hr(i)
+            })
+        );
+
         return (
-            <div>
-                <Table numRows={this.props.data.length} enableColumnReordering={true}>
-                    <Column name="IP" cellRenderer={this.renderCell}/>
-                    <Column name="History (6h)" cellRenderer={this.renderCell2}/>
-                </Table>
-                <Tabs>
-                    <Tab id="miningpool" title="Mining Pool"/>
-                    <Tab id="walletaddr" title="Wallet Address"/>
+            <div style={{ height: 500, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} checkboxSelection
+                    components={{Toolbar: Toolbar}}
+                    selectionModel={this.selected}
+                    onSelectionModelChange={sel => {
+                        this.select(sel.selectionModel);
+                        console.log(this.state.selected);
+                    }}
+                    /*onRowSelected={sel => {
+                        this.select(sel.data.ip, sel.isSelected);
+                    }}*/
+                />
+                <Tabs value={0}>
+                    <Tab label="one" index={0}/>
+                    <Tab label="two" index={1}/>
                 </Tabs>
             </div>
         );
